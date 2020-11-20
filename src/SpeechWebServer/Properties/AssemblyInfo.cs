1960259918bfd62476e5d04266cdbbab2cc030ed
ksasao <k.sasao@gmail.com>
2020-11-21 01:35:47 +0900

    fix: AssemblyInfo の内容を現状に合わせて修正

--- src/SpeechWebServer/Properties/AssemblyInfo.cs
+++ src/SpeechWebServer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Voiceroid2Enum")]
+[assembly: AssemblyTitle("SpeechWebServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Voiceroid2Enum")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("SpeechWebServer")]
+[assembly: AssemblyCopyright("Copyright ©  2020 ksasao")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
